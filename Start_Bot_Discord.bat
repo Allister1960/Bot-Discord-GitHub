6e87109327b0d04898325d638d7329e9765ec8a9
Allister1960 <aldipramanaemail@gmail.com>
2025-03-27 00:49:56 +0700

    v1.2

---
+++ Start_Bot_Discord.bat
@@ -0,0 +1,15 @@
+:0
+@echo off
+title Renren Bot
+color 0a
+cls
+echo.
+echo    ==============================
+echo     [ Bot Discord Suki na Neko ]
+echo    ==============================
+echo.
+echo.
+echo  Starting bot...    (%time%)
+echo.
+node index.js
+pause
